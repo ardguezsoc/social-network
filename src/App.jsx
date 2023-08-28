e31ca8bd731b4f58b740ce30f73b8e0e55aa67d2
ardguezsoc <adevsocorro@gmail.com>
2023-08-28 11:57:17 +0200

    core: adding context

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Root } from './routes/root';
+import { InfrastructureProvider } from './context/infraestructure';
 import NotFoundPage from './pages/NotFound';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import i18n from './i18n';
@@ -23,12 +24,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
       <QueryClientProvider client={queryClient}>
-        <Router>
-          <Routes>
-            <Route path="*" element={<NotFoundPage />} />
-            <Route path="/" element={<Root />} />
-          </Routes>
-        </Router>
+        <InfrastructureProvider>
+          <Router>
+            <Routes>
+              <Route path="*" element={<NotFoundPage />} />
+              <Route path="/" element={<Root />} />
+            </Routes>
+          </Router>
+        </InfrastructureProvider>
       </QueryClientProvider>
     </I18nextProvider>
   </React.StrictMode>
