614b09db86507017d93d0023015162e67fa7c17e
ardguezsoc <adevsocorro@gmail.com>
2023-08-28 12:10:25 +0200

    test: adding card test

--- src/components/Card/Card.test.jsx
+++ src/components/Card/Card.test.jsx
@@ -4,49 +4,35 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import { Card } from './Card';
 
 describe('Card', () => {
-  const mobileItem = {
-    imgUrl: 'path/to/image.jpg',
-    brand: 'Brand',
-    model: 'Model',
-    price: 999,
-    id: '123',
+  const userItem = {
+    id: 123,
+    name: 'Adrian',
+    userName: 'ars',
   };
 
-  test('Renders the mobile item details correctly', () => {
-    render(<Card mobileItem={mobileItem} />);
+  test('Renders the user item details correctly', () => {
+    render(<Card userItem={userItem} />);
 
-    const image = screen.getByAltText('product-image');
-    const brand = screen.getByText('Brand');
-    const model = screen.getByText('Model');
-    const price = screen.getByText('999$');
+    const name = screen.getByText('Adrian');
+    const userName = screen.getByText('ars');
     const viewMoreButton = screen.getByRole('button');
 
-    expect(image).toBeInTheDocument();
-    expect(brand).toBeInTheDocument();
-    expect(model).toBeInTheDocument();
-    expect(price).toBeInTheDocument();
+    expect(name).toBeInTheDocument();
+    expect(userName).toBeInTheDocument();
     expect(viewMoreButton).toBeInTheDocument();
   });
 
-  test('Calls the navigate function when the "View more" button is clicked', () => {
+  test('Calls the navigate function when the "Go to profile" button is clicked', () => {
     const navigateMock = vi.fn();
     vi.mock('react-router-dom', () => ({
       useNavigate: () => navigateMock,
     }));
 
-    render(<Card mobileItem={mobileItem} navigateTo={navigateMock} />);
+    render(<Card userItem={userItem} navigateTo={navigateMock} />);
 
     const viewMoreButton = screen.getByRole('button');
     fireEvent.click(viewMoreButton);
 
-    expect(navigateMock).toHaveBeenCalledWith('/mobile/123');
-  });
-
-  test('Free price is displayed when price is undefined', () => {
-    mobileItem.price = undefined;
-
-    render(<Card mobileItem={mobileItem} navigateTo={vi.fn()} />);
-
-    screen.getByText('Free');
+    expect(navigateMock).toHaveBeenCalledWith('/users/123');
   });
 });
